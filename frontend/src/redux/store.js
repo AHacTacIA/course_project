7497ba1ec1312a7b2040d198456ad779eff5e9f7
Anastasia Minchukova <anastasiaminchukova72@gmail.com>
2023-12-10 15:29:05 +0300

    authorization

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {recipeReducer} from "./slices/recipes";
-
-
+import {authReducer} from "./slices/auth";
 
 
 const store = configureStore({
     reducer: {
         recipes: recipeReducer,
+        auth: authReducer,
     }
 })
 
